ad27b068041fcc9df5480bbe49552afc03268742
alexander <alexander.t.simonsen@gmail.com>
2025-02-19 22:05:23 +0100

    musicBtn

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const menuButton = document.querySelector(".menu-toggle-btn");
 const navigationMenu = document.querySelector("nav");
 const themeToggleButton = document.getElementById("theme-toggle");
+const playButton = document.getElementById("music")
 
 // Funktion för att visa/dölja menyn
 const menuVisibility = function () {
@@ -14,10 +15,10 @@ const toggleTheme = function () {
 
     // Byt ikon mellan måne och sol
     if (document.body.classList.contains("dark-mode")) {
-        themeToggleButton.classList.replace("fa-moon", "fa-sun"); // Byt till sol-ikon 
+        themeToggleButton.classList.replace("fa-sun", "fa-moon"); // Byt till sol-ikon 
         localStorage.setItem("theme", "dark"); // Spara dark mode i localStorage
     } else {
-        themeToggleButton.classList.replace("fa-sun", "fa-moon"); // Byt till måne-ikon 
+        themeToggleButton.classList.replace("fa-moon", "fa-sun"); // Byt till måne-ikon 
         localStorage.setItem("theme", "light"); // Spara light mode i localStorage
     }
 }
@@ -25,11 +26,42 @@ const toggleTheme = function () {
 // Vid sidladdning, kontrollera om dark mode är aktivt och tillämpa det
 if (localStorage.getItem("theme") === "dark") {
     document.body.classList.add("dark-mode");
-    themeToggleButton.classList.replace("fa-moon", "fa-sun"); // Om dark mode, sätt ikon till sol
+    themeToggleButton.classList.replace("fa-sun", "fa-moon"); // Om dark mode, sätt ikon till sol
 } else {
     document.body.classList.add("light-mode");
-    themeToggleButton.classList.replace("fa-sun", "fa-moon"); // Om light mode, sätt ikon till måne
+    themeToggleButton.classList.replace("fa-moon", "fa-sun"); // Om light mode, sätt ikon till måne
 }
+
+let hour = (new Date).getHours(); // Hämta aktuell timme
+if (hour >= 19 || hour < 7) { 
+    document.body.classList.add("dark-mode");
+    themeToggleButton.classList.replace("fa-sun", "fa-moon");
+}
+
+
+let audio = new Audio("music/deep-future-garage-royalty-free-music-163081.mp3");
+audio.loop = true;
+//kontrollera om musik ska fortsätta spelas på andra flikar
+if (sessionStorage.getItem("isPlaying") === "true") {
+        audio.play();
+    }
+
+
+// Eventlyssnare för att spela/pausa musik
+playButton.addEventListener("click",() => {
+    if (audio.paused){
+        audio.play();
+        sessionStorage.setItem("isPlaying", "true");
+    } else {
+        audio.pause();
+        sessionStorage.setItem("isPlaying", "false");
+    }});
+
+    //spara musikstatus
+    window.addEventListener("beforeunload", () => {
+        sessionStorage.setItem("isPlaying", audio.paused ? "false" : "true");
+    });
+
 
 // Lägg till event-lyssnare
 menuButton.addEventListener("click", menuVisibility);
